25fa84a48358c3ad1dc32b249e5d95a1843fdd94
nnydtmg <nnydtmg@gmail.com>
2023-02-03 23:29:47 +0900

    Initial commit

---
+++ test/aws-1vpc-3private-subnet.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Aws1Vpc3PrivateSubnet from '../lib/aws-1vpc-3private-subnet-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-1vpc-3private-subnet-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Aws1Vpc3PrivateSubnet.Aws1Vpc3PrivateSubnetStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
