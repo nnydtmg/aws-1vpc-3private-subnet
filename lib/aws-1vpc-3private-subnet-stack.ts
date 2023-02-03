d063038512f23007d383739374240e4698b9bb09
nnydtmg <nnydtmg@gmail.com>
2023-02-04 00:43:46 +0900

    Add test module

--- lib/aws-1vpc-3private-subnet-stack.ts
+++ lib/aws-1vpc-3private-subnet-stack.ts
@@ -1,16 +1,53 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from '@aws-cdk/core';
+import { CfnVPC, CfnSubnet } from '@aws-cdk/aws-ec2';
 
-export class Aws1Vpc3PrivateSubnetStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+export class DevioStack extends cdk.Stack {
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const systemName = this.node.tryGetContext('systemName');
+    const envType = this.node.tryGetContext('envType');
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'Aws1Vpc3PrivateSubnetQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const vpc = new CfnVPC(this, 'Vpc', {
+      cidrBlock: '10.0.0.0/16',
+      tags: [{ key: 'Name', value: `${systemName}-${envType}-vpc` }]
+    });
+
+    const subnetPublic1a = new CfnSubnet(this, 'SubnetPublic1a', {
+      cidrBlock: '10.0.11.0/24',
+      vpcId: vpc.ref,
+      availabilityZone: 'ap-northeast-1a',
+      tags: [{ key: 'Name', value: `${systemName}-${envType}-subnet-public-1a` }]
+    })
+    const subnetPublic1c = new CfnSubnet(this, 'SubnetPublic1c', {
+      cidrBlock: '10.0.12.0/24',
+      vpcId: vpc.ref,
+      availabilityZone: 'ap-northeast-1c',
+      tags: [{ key: 'Name', value: `${systemName}-${envType}-subnet-public-1c` }]
+    })
+    const subnetApp1a = new CfnSubnet(this, 'SubnetApp1a', {
+      cidrBlock: '10.0.21.0/24',
+      vpcId: vpc.ref,
+      availabilityZone: 'ap-northeast-1a',
+      tags: [{ key: 'Name', value: `${systemName}-${envType}-subnet-app-1a` }]
+    })
+    const subnetApp1c = new CfnSubnet(this, 'SubnetApp1c', {
+      cidrBlock: '10.0.22.0/24',
+      vpcId: vpc.ref,
+      availabilityZone: 'ap-northeast-1c',
+      tags: [{ key: 'Name', value: `${systemName}-${envType}-subnet-app-1c` }]
+    })
+    const subnetDb1a = new CfnSubnet(this, 'SubnetDb1a', {
+      cidrBlock: '10.0.31.0/24',
+      vpcId: vpc.ref,
+      availabilityZone: 'ap-northeast-1a',
+      tags: [{ key: 'Name', value: `${systemName}-${envType}-subnet-db-1a` }]
+    })
+    const subnetDb1c = new CfnSubnet(this, 'SubnetDb1c', {
+      cidrBlock: '10.0.32.0/24',
+      vpcId: vpc.ref,
+      availabilityZone: 'ap-northeast-1c',
+      tags: [{ key: 'Name', value: `${systemName}-${envType}-subnet-db-1c` }]
+    })
   }
 }
--- test/aws-1vpc-3private-subnet.test.ts
+++ test/aws-1vpc-3private-subnet.test.ts
@@ -1,17 +1,65 @@
-// import * as cdk from 'aws-cdk-lib';
-// import { Template } from 'aws-cdk-lib/assertions';
-// import * as Aws1Vpc3PrivateSubnet from '../lib/aws-1vpc-3private-subnet-stack';
+import { expect, countResources, haveResource } from '@aws-cdk/assertions';
+import * as cdk from '@aws-cdk/core';
+import * as Devio from '../lib/aws-1vpc-3private-subnet-stack';
 
-// example test. To run these tests, uncomment this file along with the
-// example resource in lib/aws-1vpc-3private-subnet-stack.ts
-test('SQS Queue Created', () => {
-//   const app = new cdk.App();
-//     // WHEN
-//   const stack = new Aws1Vpc3PrivateSubnet.Aws1Vpc3PrivateSubnetStack(app, 'MyTestStack');
-//     // THEN
-//   const template = Template.fromStack(stack);
+test('Context', () => {
+  const app = new cdk.App({
+    context: {
+      'systemName': 'starwars',
+      'envType': 'prd'
+    }
+  });
+  const stack = new Devio.DevioStack(app, 'DevioStack');
 
-//   template.hasResourceProperties('AWS::SQS::Queue', {
-//     VisibilityTimeout: 300
-//   });
+  expect(stack).to(haveResource('AWS::EC2::VPC', {
+    Tags: [{ 'Key': 'Name', 'Value': 'starwars-prd-vpc' }]
+  }));
+});
+
+test('Vpc', () => {
+  const app = new cdk.App();
+  const stack = new Devio.DevioStack(app, 'DevioStack');
+
+  expect(stack).to(countResources('AWS::EC2::VPC', 1));
+  expect(stack).to(haveResource('AWS::EC2::VPC', {
+    CidrBlock: '10.0.0.0/16',
+    Tags: [{ 'Key': 'Name', 'Value': 'undefined-undefined-vpc' }]
+  }));
+});
+
+test('Subnet', () => {
+  const app = new cdk.App();
+  const stack = new Devio.DevioStack(app, 'DevioStack');
+
+  expect(stack).to(countResources('AWS::EC2::Subnet', 6));
+  expect(stack).to(haveResource('AWS::EC2::Subnet', {
+    CidrBlock: '10.0.11.0/24',
+    AvailabilityZone: 'ap-northeast-1a',
+    Tags: [{ 'Key': 'Name', 'Value': 'undefined-undefined-subnet-public-1a' }]
+  }));
+  expect(stack).to(haveResource('AWS::EC2::Subnet', {
+    CidrBlock: '10.0.12.0/24',
+    AvailabilityZone: 'ap-northeast-1c',
+    Tags: [{ 'Key': 'Name', 'Value': 'undefined-undefined-subnet-public-1c' }]
+  }));
+  expect(stack).to(haveResource('AWS::EC2::Subnet', {
+    CidrBlock: '10.0.21.0/24',
+    AvailabilityZone: 'ap-northeast-1a',
+    Tags: [{ 'Key': 'Name', 'Value': 'undefined-undefined-subnet-app-1a' }]
+  }));
+  expect(stack).to(haveResource('AWS::EC2::Subnet', {
+    CidrBlock: '10.0.22.0/24',
+    AvailabilityZone: 'ap-northeast-1c',
+    Tags: [{ 'Key': 'Name', 'Value': 'undefined-undefined-subnet-app-1c' }]
+  }));
+  expect(stack).to(haveResource('AWS::EC2::Subnet', {
+    CidrBlock: '10.0.31.0/24',
+    AvailabilityZone: 'ap-northeast-1a',
+    Tags: [{ 'Key': 'Name', 'Value': 'undefined-undefined-subnet-db-1a' }]
+  }));
+  expect(stack).to(haveResource('AWS::EC2::Subnet', {
+    CidrBlock: '10.0.32.0/24',
+    AvailabilityZone: 'ap-northeast-1c',
+    Tags: [{ 'Key': 'Name', 'Value': 'undefined-undefined-subnet-db-1c' }]
+  }));
 });
